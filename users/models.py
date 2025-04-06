80ddb99f2292798b4b140c86df99c986e391b2c9
K-Krut <katiakrut448@gmail.com>
2025-04-06 21:44:52 +0300

    CustomUserManager added

--- users/models.py
+++ users/models.py
@@ -1,6 +1,39 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, first_name=None, last_name=None):
+        if not email:
+            raise ValueError('Email is required')
+        if not password:
+            raise ValueError('Password is required')
+        if not first_name:
+            raise ValueError('First Name is required')
+        if not last_name:
+            raise ValueError('Last Name is required')
+
+        user = self.model(email=self.normalize_email(email), first_name=first_name, last_name=last_name)
+        user.set_password(password)
+
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, first_name=None, last_name=None):
+        user = self.create_user(email, password, first_name, last_name)
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, email, password, first_name=None, last_name=None):
+        user = self.create_user(email, password, first_name, last_name)
+        user.is_staff = True
+
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
