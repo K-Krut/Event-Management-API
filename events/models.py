8f86d61d7782d5f082a86621e2a75babeab765ca
K-Krut <katiakrut448@gmail.com>
2025-04-07 17:34:53 +0300

    event model added

--- events/models.py
+++ events/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+from users.models import User
 
-class EventType(models.Model):
+
+class Type(models.Model):
     name = models.CharField(unique=True, max_length=100)
 
     def __str__(self):
@@ -9,8 +11,32 @@ class EventType(models.Model):
 
 
 
-class EventStatus(models.Model):
+class Status(models.Model):
     name = models.CharField(unique=True, max_length=100)
 
     def __str__(self):
         return self.name
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT,  related_name='events')
+    type = models.ForeignKey(Type, on_delete=models.PROTECT,  related_name='events')
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='organizer')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date_start']
+        indexes = [
+            models.Index(fields=['-date_start']),
+        ]
+
+    def __str__(self):
+        return self.title
