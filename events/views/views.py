7a56659f778800bc9db1ae1ee33e9b51a6ba1395
K-Krut <katiakrut448@gmail.com>
2025-04-08 01:27:14 +0300

    events creation added

--- events/views/views.py
+++ events/views/views.py
@@ -1,9 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from events.constants import EVENT_STATUSES_EXCLUDED_IN_LIST
 from events.decorators import server_exception
-from events.models import Event
+from events.models import Event, EventParticipants
+from events.serializers import EventCreateSerializer
 from events.views.mixins import EventListMixin
 
 
@@ -30,3 +32,16 @@ class EventMyOrganizedListView(EventListMixin, generics.ListAPIView):
     def get_queryset(self):
         return Event.objects.exclude(status__name__in=EVENT_STATUSES_EXCLUDED_IN_LIST).filter(
             organizer=self.request.user).order_by(*self.ordering)
+
+
+class EventCreateView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = EventCreateSerializer
+
+    @server_exception
+    def perform_create(self, serializer):
+        user = self.request.user
+        event = serializer.save(organizer=user)
+        EventParticipants.objects.create(event=event, user=user)
+        # response = EventSerializer(event, context={'request': self.request})
+        return Response(event, status=status.HTTP_201_CREATED)
