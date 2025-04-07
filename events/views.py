c510dc73fe28b43dd468111a6173db11f3129453
K-Krut <katiakrut448@gmail.com>
2025-04-07 21:03:40 +0300

    simple events list view added

--- events/views.py
+++ events/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from events.models import Event
+from events.serializers import EventSerializer
 
 
 class EventListView(generics.ListAPIView):
-    pass
+    serializer_class = EventSerializer
+
+    def get_queryset(self):
+        try:
+            return Event.objects.all()
+        except Exception as error:
+            return Response({'errors': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
