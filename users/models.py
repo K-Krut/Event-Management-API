682e97d099a5a22ea1cf9c93256a380aab42cb67
K-Krut <katiakrut448@gmail.com>
2025-04-07 13:36:16 +0300

    fixed CustomUserManager

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, first_name=None, last_name=None):
+    def create_user(self, email, password=None, first_name=None, last_name=None, avatar=None):
         if not email:
             raise ValueError('Email is required')
         if not password:
@@ -14,22 +14,27 @@ class CustomUserManager(BaseUserManager):
         if not last_name:
             raise ValueError('Last Name is required')
 
-        user = self.model(email=self.normalize_email(email), first_name=first_name, last_name=last_name)
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            avatar=avatar
+        )
         user.set_password(password)
 
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password, first_name=None, last_name=None):
-        user = self.create_user(email, password, first_name, last_name)
+    def create_superuser(self, email, password, first_name=None, last_name=None, avatar=None):
+        user = self.create_user(email, password, first_name, last_name, avatar)
         user.is_staff = True
         user.is_superuser = True
 
         user.save(using=self._db)
         return user
 
-    def create_staffuser(self, email, password, first_name=None, last_name=None):
-        user = self.create_user(email, password, first_name, last_name)
+    def create_staffuser(self, email, password, first_name=None, last_name=None, avatar=None):
+        user = self.create_user(email, password, first_name, last_name, avatar)
         user.is_staff = True
 
         user.save(using=self._db)
