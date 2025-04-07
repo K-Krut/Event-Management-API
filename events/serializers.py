de94db9feb1c07f1526defa8f17dca946e83a9ef
K-Krut <katiakrut448@gmail.com>
2025-04-08 00:48:27 +0300

    is registered feature added

--- events/serializers.py
+++ events/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from events.models import Event
+from events.models import Event, EventParticipants
 
 
 class EventSerializer(serializers.ModelSerializer):
+    is_registered = serializers.SerializerMethodField()
 
     class Meta:
         model = Event
-        fields = ['id', 'title', 'date_start', 'date_end', 'status']
+        fields = ['id', 'title', 'date_start', 'date_end', 'status', 'is_registered']
+
+    def get_is_registered(self, obj):
+        user = self.context['request'].user
+
+        return False if not user.is_authenticated else EventParticipants.objects.filter(user=user, event=obj).exists()
+
