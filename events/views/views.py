b0351fb817128cdb33ca3127e59762052cd4d277
K-Krut <katiakrut448@gmail.com>
2025-04-07 22:11:00 +0300

    refactored event list views

--- events/views/views.py
+++ events/views/views.py
@@ -1,26 +1,12 @@
 from rest_framework import generics, status, filters
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from events.models import Event
 from events.serializers import EventSerializer
+from events.views.mixins import EventListMixin
 
 
-class Pagination(PageNumberPagination):
-    page_size = 30
-    page_size_query_param = 'page_size'
-    max_page_size = 100
-
-
-class EventListView(generics.ListAPIView):
-    serializer_class = EventSerializer
-
-    pagination_class = Pagination
-    ordering_fields = ['title', 'date_start', 'date_end', 'status']
-    ordering = ['date_start', 'status']
-
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ['title']
+class EventListView(EventListMixin, generics.ListAPIView):
 
     def get_queryset(self):
         try:
@@ -29,16 +15,8 @@ class EventListView(generics.ListAPIView):
             return Response({'errors': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-class EventMyListView(generics.ListAPIView):
+class EventMyListView(EventListMixin, generics.ListAPIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = EventSerializer
-
-    pagination_class = Pagination
-    ordering_fields = ['title', 'date_start', 'date_end', 'status']
-    ordering = ['date_start', 'status']
-
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ['title']
 
     def get_queryset(self):
         try:
@@ -48,22 +26,12 @@ class EventMyListView(generics.ListAPIView):
             return Response({'errors': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-class EventMyOrganizedListView(generics.ListAPIView):
+class EventMyOrganizedListView(EventListMixin, generics.ListAPIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = EventSerializer
-
-    pagination_class = Pagination
-    ordering_fields = ['title', 'date_start', 'date_end', 'status']
-    ordering = ['date_start', 'status']
-
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ['title']
 
     def get_queryset(self):
         try:
-            return (
-                Event.objects.exclude(status__name__in=['Draft', 'Canceled']).filter(
-                    organizer=self.request.user).order_by(*self.ordering)
-            )
+            return Event.objects.exclude(status__name__in=['Draft', 'Canceled']).filter(
+                organizer=self.request.user).order_by(*self.ordering)
         except Exception as error:
             return Response({'errors': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
