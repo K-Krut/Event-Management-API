041d4e7ea98679251e200135ac1bddc235f71ffe
K-Krut <katiakrut448@gmail.com>
2025-04-08 01:56:40 +0300

    added decorator for base event exceptions

--- events/decorators.py
+++ events/decorators.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
 from rest_framework import status
 from functools import wraps
@@ -8,6 +9,21 @@ def server_exception(func):
     def wrapper(self, *args, **kwargs):
         try:
             return func(self, *args, **kwargs)
+        except Exception as error:
+            return Response({'errors': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    return wrapper
+
+
+def event_exceptions(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except ObjectDoesNotExist:
+            return Response({'errors': 'Event not found'}, status=status.HTTP_404_NOT_FOUND)
+        except TimeoutError:
+            return Response({'errors': 'Request timeout'}, status=status.HTTP_504_GATEWAY_TIMEOUT)
         except Exception as error:
             return Response({'errors': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
