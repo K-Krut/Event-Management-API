7af6f5d8e3a6fdd0677b4fae06715a103d15a2fb
K-Krut <katiakrut448@gmail.com>
2025-04-07 13:36:03 +0300

    added user admin model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.utils import formats
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        'email', 'first_name', 'last_name', 'email_notifications', 'formatted_registration_date', 'is_superuser',
+        'is_staff', 'is_active'
+    ]
+    list_filter = ['email_notifications', 'created_at', 'is_superuser', 'is_staff', 'is_active']
+    search_fields = ['first_name', 'last_name', 'email']
+    ordering = ['created_at', 'first_name', 'last_name']
+    show_facets = admin.ShowFacets.ALWAYS
+
+    def formatted_registration_date(self, obj):
+        return formats.date_format(obj.created_at, "Y-m-d")
+
+    formatted_registration_date.short_description = 'Registration Date'
