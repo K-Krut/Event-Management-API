5b7189dc7983ea54bff3b2bd167f4e5b9bc6f090
K-Krut <katiakrut448@gmail.com>
2025-04-07 17:27:52 +0300

    added event types and statuses models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class EventType(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+
+class EventStatus(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+
+    def __str__(self):
+        return self.name
