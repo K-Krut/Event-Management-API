9c06a4c553c0f200b365fda1591e387602578ab1
K-Krut <katiakrut448@gmail.com>
2025-04-07 22:17:24 +0300

    refactored event list views with decorators

--- events/views/views.py
+++ events/views/views.py
@@ -1,37 +1,31 @@
-from rest_framework import generics, status, filters
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+
+from events.decorators import server_exception
 from events.models import Event
-from events.serializers import EventSerializer
 from events.views.mixins import EventListMixin
 
 
 class EventListView(EventListMixin, generics.ListAPIView):
 
+    @server_exception
     def get_queryset(self):
-        try:
-            return Event.objects.exclude(status__name__in=['Draft', 'Canceled']).order_by(*self.ordering)
-        except Exception as error:
-            return Response({'errors': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Event.objects.exclude(status__name__in=['Draft', 'Canceled']).order_by(*self.ordering)
 
 
 class EventMyListView(EventListMixin, generics.ListAPIView):
     permission_classes = [IsAuthenticated]
 
+    @server_exception
     def get_queryset(self):
-        try:
-            return Event.objects.exclude(status__name__in=['Draft', 'Canceled']).filter(
-                event__user=self.request.user).order_by(*self.ordering)
-        except Exception as error:
-            return Response({'errors': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Event.objects.exclude(status__name__in=['Draft', 'Canceled']).filter(
+            event__user=self.request.user).order_by(*self.ordering)
 
 
 class EventMyOrganizedListView(EventListMixin, generics.ListAPIView):
     permission_classes = [IsAuthenticated]
 
+    @server_exception
     def get_queryset(self):
-        try:
-            return Event.objects.exclude(status__name__in=['Draft', 'Canceled']).filter(
-                organizer=self.request.user).order_by(*self.ordering)
-        except Exception as error:
-            return Response({'errors': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Event.objects.exclude(status__name__in=['Draft', 'Canceled']).filter(
+            organizer=self.request.user).order_by(*self.ordering)
