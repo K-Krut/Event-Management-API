030a4e9dd89c6e7eaf863a2a2b6ee9b33b9daa28
K-Krut <katiakrut448@gmail.com>
2025-04-07 21:07:47 +0300

    filtered events

--- events/views.py
+++ events/views.py
@@ -16,7 +16,7 @@ class EventListView(generics.ListAPIView):
 
     pagination_class = Pagination
     ordering_fields = ['title', 'date_start', 'date_end', 'status']
-    ordering = ['-date_start', 'status', ]
+    ordering = ['-date_start', 'status']
 
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['title']
@@ -24,6 +24,6 @@ class EventListView(generics.ListAPIView):
 
     def get_queryset(self):
         try:
-            return Event.objects.all()
+            return Event.objects.exclude(status__name__in=['Draft', 'Canceled']).order_by(*self.ordering)
         except Exception as error:
             return Response({'errors': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
