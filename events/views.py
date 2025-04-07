f7099f29b4c19bd66a7e0778cf356b04cbfee289
K-Krut <katiakrut448@gmail.com>
2025-04-07 21:06:07 +0300

    added pagination class

--- events/views.py
+++ events/views.py
@@ -1,11 +1,26 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, filters
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from events.models import Event
 from events.serializers import EventSerializer
 
 
+class Pagination(PageNumberPagination):
+    page_size = 30
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
 class EventListView(generics.ListAPIView):
     serializer_class = EventSerializer
+
+    pagination_class = Pagination
+    ordering_fields = ['title', 'date_start', 'date_end', 'status']
+    ordering = ['-date_start', 'status', ]
+
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title']
+
 
     def get_queryset(self):
         try:
