f4adf9ec2ab472e889e867b4ca2c1646e5415ea2
K-Krut <katiakrut448@gmail.com>
2025-04-08 02:02:40 +0300

    event details view & url added

--- events/urls.py
+++ events/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from events.views.views import EventListView, EventMyListView, EventMyOrganizedListView, EventCreateView
+from events.views.views import EventListView, EventMyListView, EventMyOrganizedListView, EventCreateView, \
+    EventDetailView
 
 urlpatterns = [
     path('events/', EventListView.as_view(), name='events'),
-    path('events/create/', EventCreateView.as_view(), name='events'),
-    path('events/my/', EventMyListView.as_view(), name='events_my'),
-    path('events/my/organized', EventMyOrganizedListView.as_view(), name='events_my_organized'),
+    path('events/create/', EventCreateView.as_view(), name='events-create'),
+    path('events/my/', EventMyListView.as_view(), name='events-my'),
+    path('events/my/organized', EventMyOrganizedListView.as_view(), name='events-my-organized'),
+    path('events/<int:id>/', EventDetailView.as_view(), name='event-details'),
+
 ]
--- events/views/views.py
+++ events/views/views.py
@@ -1,11 +1,13 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from events.constants import EVENT_STATUSES_EXCLUDED_IN_LIST
-from events.decorators import server_exception
+from events.decorators import server_exception, event_exceptions
 from events.models import Event, EventParticipants
-from events.serializers import EventCreateSerializer
+from events.serializers import EventCreateSerializer, EventDetailsSerializer
 from events.views.mixins import EventListMixin
 
 
@@ -45,3 +47,16 @@ class EventCreateView(generics.CreateAPIView):
         EventParticipants.objects.create(event=event, user=user)
         # response = EventSerializer(event, context={'request': self.request})
         return Response(event, status=status.HTTP_201_CREATED)
+
+
+class EventDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = EventDetailsSerializer
+
+    @event_exceptions
+    def get(self, request, *args, **kwargs):
+        event_id = kwargs.get('id')
+        event = Event.objects.get(id=event_id)
+        response = self.serializer_class(event, context={'request': request})
+        return Response(response.data, status=status.HTTP_200_OK)
+
