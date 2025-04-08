9ca0abfdb5019048c385fad9453a209bc58333a3
K-Krut <katiakrut448@gmail.com>
2025-04-08 14:43:15 +0300

    swagger docs added

--- EventManagementAPI/settings.py
+++ EventManagementAPI/settings.py
@@ -29,7 +29,9 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework_simplejwt',
-    'rest_framework_simplejwt.token_blacklist'
+    'rest_framework_simplejwt.token_blacklist',
+
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -93,6 +95,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 SIMPLE_JWT = {
