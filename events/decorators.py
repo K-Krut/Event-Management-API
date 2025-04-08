12fa89082391ba33f6e3030092765f3d82ea12b0
K-Krut <katiakrut448@gmail.com>
2025-04-08 11:03:48 +0300

    added decorator for checking if current user is organizer (permissions check)

--- events/decorators.py
+++ events/decorators.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from functools import wraps
 
+from events.models import Event
+
 
 def server_exception(func):
     @wraps(func)
@@ -26,5 +28,19 @@ def event_exceptions(func):
             return Response({'errors': 'Request timeout'}, status=status.HTTP_504_GATEWAY_TIMEOUT)
         except Exception as error:
             return Response({'errors': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    return wrapper
+
+
+def organizer_required(func):
+    @wraps(func)
+    def wrapper(self, request, *args, **kwargs):
+        event_id = kwargs.get('id')
+        event = Event.objects.get(id=event_id)
+        if event.organizer != request.user:
+            return Response({'errors': 'Access forbidden'}, status=status.HTTP_403_FORBIDDEN)
+
+        self.event = event
+        return func(self, request, *args, **kwargs)
 
     return wrapper
