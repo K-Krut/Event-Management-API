7fb29cc998aa5cf7f9ebfd259e20be903da64646
K-Krut <katiakrut448@gmail.com>
2025-04-08 15:18:45 +0300

    added redirect to docs

--- EventManagementAPI/urls.py
+++ EventManagementAPI/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('api/v1/', include('events.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', RedirectView.as_view(url='/api/docs/', permanent=False)),
 ]
